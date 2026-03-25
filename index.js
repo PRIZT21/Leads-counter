32a574c61fa6134a0af7db3534f90f19437979ae
PRIZT <ayobamiji2004@gmail.com>
2026-03-25 02:12:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-El")
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+    // inputEl.value = ""
+    
+})
+
+// function renderLead(){
+//     let listItem = "<li>" + inputEl.value + "</li>"
+//     ulEl.innerHTML= listItem
+// }
+
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++){
+        // listItems += "<li> <a  target='_blank' href='" + myLeads[i] +"'>" + myLeads[i] + "</a> </li>"
+        listItems += `
+            <li> 
+                <a  target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]} 
+                </a> 
+            </li>
+        `
+        //   const li = document.createElement("li")
+        //   li.textContent = myLeads[i]
+        //   ulEl.append(li)
+    }
+
+    ulEl.innerHTML= listItems
+}
